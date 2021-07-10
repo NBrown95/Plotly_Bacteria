07f539148ce7f051fc894b9cea3dc1eb2bee59f9
NBrown95 <natebrown918@gmail.com>
2021-07-09 20:57:56 -0500

    Added gauge chart.

--- charts.js
+++ charts.js
@@ -116,5 +116,37 @@ function buildCharts(sample) {
 
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+  
+    // Deliverable 3:
+    // 3. Create a variable that holds the washing frequency.
+    var frequency = float(result.wfreq);
+    // 4. Create the trace for the gauge chart.
+    var trace3 = {
+      value: frequency,
+      title: {text: "Scrubs per Week"},
+      type:"indicator",
+      mode:"gauge+number",
+      gauge: {
+        axis: {range: [0, 10]},
+        bar: {color: "black"},
+        steps: [
+          {range: [0,2], color: "red"},
+          {range: [2,4], color: "orange"},
+          {range: [4,6], color: "yellow"},
+          {range: [6,8], color: "lightgreen"},
+          {range: [8,10], color: "darkgreen"}
+        ]
+      }
+    };
+
+    var gaugeData = [trace3];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+      title: "Belly Button Washing Frequency"
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
 }
